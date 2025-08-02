b95bac7424f608833f68b9c3269c15476fa3a76c
Nada Hossam Ismail <nadahossamismail@gmail.com>
2025-08-02 18:23:00 +0300

    Initialize MVVM folder structure

---
+++ lib/core/app_routes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/features/home/presentation/home_view.dart';
+
+class Routes {
+  static const String homeRoute = "/home";
+}
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.homeRoute:
+        return MaterialPageRoute(builder: (_) => const HomeView());
+
+      default:
+        return undefinedRoute();
+    }
+  }
+
+  static Route<dynamic> undefinedRoute() {
+    return MaterialPageRoute(
+      builder: (_) => const Scaffold(body: Center(child: Text("Not Found"))),
+    );
+  }
+}
